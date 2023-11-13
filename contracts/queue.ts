2d752495a20328ec092d2b4ed661ab4690dee2da
nicole595 <nicoletp16@gmal.com>
2023-11-13 10:48:41 -0300

    girl

--- contracts/queue.ts
+++ contracts/queue.ts
@@ -1,4 +1,6 @@
+import type { SendPushPayload } from "App/Jobs/SendPush";
 declare module '@ioc:Rlanz/Queue' {
   interface JobsList {
+    'App/Jobs/SendPush': SendPushPayload
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -24,5 +24,9 @@ Route.get('/', async () => {
   return { hello: 'world' }
 })
 
-Route.post('/register', 'AuthController.register')
-Route.post('/login', 'AuthController.login')
+Route.post("/register", "AuthController.register")
+Route.post("/login", "AuthController.login")
+Route .group(() => {
+  Route.put("/user", "UsersController.update")
+  Route.post("/push", "SendPushesController.send")
+}).middleware('auth');
